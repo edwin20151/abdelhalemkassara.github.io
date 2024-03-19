75a79ef401f2aa34a104458ada238b384719103e
LiQian Zhou <90808836+edwin20151@users.noreply.github.com>
2024-03-19 00:45:43 -0400

    update

---
+++ frontend/src/pages/DataAnalysisTool/DependentSelection/DropdownMenu.tsx
@@ -0,0 +1,63 @@
+import { useContext, useEffect, useState, useRef } from 'react';
+import { Select, MenuItem, SelectChangeEvent, InputLabel, FormControl } from '@mui/material';
+import { datasetQuery } from "../../../components/DatabaseContext";
+import { QuestionId, QuestionText } from '../../../components/NewTypes';
+
+function DropdownMenu({ dataset, setDependentQuestion, depVar }: { dataset: string | undefined, setDependentQuestion: React.Dispatch<React.SetStateAction<string | undefined>>, depVar: string | undefined }): JSX.Element {
+  const [questions, setQuestions] = useState<Map<QuestionId, QuestionText>>();
+  const datasetQ = useContext(datasetQuery);
+  const elementRef = useRef(null);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      if (dataset) {
+        const questionsData = await datasetQ.getDependentQuestions(dataset);
+        setQuestions(questionsData);
+      } else {
+        setQuestions(new Map());
+        setDependentQuestion(undefined);
+
+      }
+
+    }
+    fetchData();
+  }, [dataset]);
+
+  const handleSelectChange = (event: SelectChangeEvent<string>) => {
+    setDependentQuestion(event.target.value);
+  };
+
+  return (
+    <FormControl fullWidth ref={elementRef}>
+      <InputLabel id="demo-simple-select-label">Dependant Variables</InputLabel>
+      <Select
+        value={depVar ? depVar : ''}
+        onChange={handleSelectChange}
+        fullWidth
+        label="Dependant Variables"
+      >
+        {(
+          () => {
+            let out: JSX.Element[] = [];
+            if (questions) {
+              for (let [key, value] of questions) {
+                const currWidth = elementRef?.current?.offsetWidth || 0
+                out.push(<MenuItem key={key} value={key}>
+                  <span style={{ width: currWidth === 0 ? '100%' : `${currWidth}px`, whiteSpace: 'wrap' }}>
+                    {value}
+                  </span>
+                </MenuItem>
+                );
+              }
+            }
+            return out;
+          })()}
+      </Select>
+    </FormControl >
+
+  );
+}
+
+export default DropdownMenu;
+
---
+++ frontend/src/pages/DataAnalysisTool/components/FilterButtons.tsx
@@ -0,0 +1,65 @@
+import { useContext, useEffect, useState } from "react";
+import { datasetQuery } from '../../../components/DatabaseContext';
+
+interface FilterButtonsProps {
+  dataset: string | undefined;
+  indVar: string | undefined;
+  setSelectedButton: React.Dispatch<React.SetStateAction<string[] | undefined>>;
+}
+
+export default function FilterButtons({ dataset, indVar, setSelectedButton }: FilterButtonsProps): JSX.Element {
+  const datasetQ = useContext(datasetQuery);
+
+  const [indVarAnswrCnt, setIndVarAnswrCnt] = useState<Map<string, boolean>>(new Map());
+
+  useEffect(() => {
+    if (dataset && indVar) {
+      datasetQ.getAnswersCount(dataset, indVar).then((val: Map<string, number>) => {
+        const initialMap: Map<string, boolean> = new Map();
+
+        val.forEach((_, key) => {
+          initialMap.set(key, false);
+        });
+        setIndVarAnswrCnt(initialMap);
+      });
+    } else {
+      setIndVarAnswrCnt(new Map());
+    }
+  }, [dataset, indVar]);
+
+ const handleButtonClick = (key: string) => {
+let ref;
+    setIndVarAnswrCnt((prevState) => {
+      const newState = new Map(prevState);
+      newState.set(key, !prevState.get(key));
+      //console.log(newState);
+ref = newState;     
+return newState;
+    });
+
+    // Update selected filters based on the new state directly
+    const updatedArray = Array.from(ref)
+      .filter(([, isSelected]) => isSelected)
+      .map(([key]) => key);
+
+    // console.log("Updated Array:", updatedArray);
+    setSelectedButton(updatedArray);
+  };
+
+  return (
+    <>
+      <p>Select filter(s):</p>
+      <div className="data_filter_buttons">
+        {Array.from(indVarAnswrCnt).map(([key, isSelected]) => (
+          <button
+            key={key}
+            onClick={() => handleButtonClick(key)}
+            style={{ backgroundColor: isSelected ? 'gray' : '#fbc02d' }}
+          >
+            {key}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
